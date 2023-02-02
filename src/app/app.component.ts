7e1ddf9e94e1d613dc0d8d449810737f75b35f2a
saurabhsharmaultra <saurabh.sharma@ultra.io>
2023-02-02 15:53:49 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lucca-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'lucca-assessment';
+}
